6ea79cfc29597940cddfc3ffb118f6b79ed0e092
AndrewKryvenko <124074819+AndrewKryvenko@users.noreply.github.com>
2024-03-26 14:13:13 +0200

    keyboards.py

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,11 +1,15 @@
 from aiogram.types import WebAppInfo
 from aiogram import types
 
-web_app = WebAppInfo(url='site_url')
+web_app = WebAppInfo(url='https://dyplom1.github.io/')
 
 keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
         [types.KeyboardButton(text='Site', web_app=web_app)]
     ],
     resize_keyboard=True
+)
+cb = CallbackData('btn', 'action')
+key = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton('Pay', callback_data='btn:buy')]]
 )
